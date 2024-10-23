cc607eec4acc28363e5afd71c47fbd227dc6f4df
Karolina Tchiling <kdt@my.yorku.ca>
2024-10-23 15:57:36 -0400

    Inital implementation of navigation bar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,73 @@
+
+import React from 'react';  // Ensure React is imported if using JSX
+import cart from '../assets/cart.svg';  
+import person from '../assets/person.svg'
+
+// Define a type for the navigation links
+interface NavLink {
+    name: string;
+    href: string;
+  }
+  
+  // Sample navigation links
+  const navLinks: NavLink[] = [
+    { name: 'Merch', href: '/' },
+    { name: 'Sale', href: '/shop' },
+    { name: 'Gift Cards', href: '/about' },
+    { name: 'Contact', href: '/contact' }
+  ];
+  
+  const Navbar: React.FC = () => {
+    return (
+      <nav className="bg-tea-800 p-10"> 
+
+        <ul className="flex flex-row text-center items-center">
+
+
+          {/* DJ WAMP Logo */}
+          <li className="basis-[20%] flex justify-start"
+          style={{
+            color: "#000",
+            fontFamily: "'Lexend Zetta', sans-serif",
+            fontSize: "25px",
+            fontStyle: "normal",
+            fontWeight: 400,
+            lineHeight: "normal",
+          }}>DJ WAMP</li>
+
+          {/* Page Links */}
+          <li className="basis-[54%] pr-[90px]">
+            <ul className="flex flex-row text-center w-full">
+              <li className="flex-grow border-[1px] border-r-[0px] border-camel py-[5px]">Merch</li>
+              <li className="flex-grow border-[1px] border-r-[0px] border-camel py-[5px]">Sale</li>
+              <li className="flex-grow border-[1px] border-r-[0px] border-camel py-[5px]">Gift Cards</li>
+              <li className="flex-grow border-[1px] border-camel py-[5px]">Contact</li>
+            </ul>
+          </li>
+
+          {/* Shopping Cart */}
+          <li className="basis-[3%] flex justify-start items-center">
+            <img src={cart} alt="Shopping Cart" />
+          </li>
+
+          {/* Profile */}
+          <li className="basis-[3%] flex justify-end items-center">
+            <img src={person} alt="Profile" className="text-center" />
+          </li>
+
+          {/* Search Bar */}
+          <li className="basis-[20%] flex items-center space-x-2 pl-[30px]">
+            <span>Search</span>
+            <input
+              type="text"
+              className="border border-camel bg-transparent p-1 flex-grow w-full"
+            />
+          </li>
+        </ul>
+          
+      </nav>
+    );
+  };
+
+  
+  export default Navbar;
