c7e1bee007e940dd4561967755c343a834040d8b
Karolina Tchiling <kdt@my.yorku.ca>
2024-10-23 18:00:52 -0400

    Implementation of sidebar

--- frontend/src/pages/MerchPage.tsx
+++ frontend/src/pages/MerchPage.tsx
@@ -1,10 +1,13 @@
-
 import React from 'react';
+import Sidebar from '../components/Sidebar';
 
 const MerchPage: React.FC = () => {
   return (
-    <div>
-      <h1>Welcome to the Merch Page!</h1> {/* Ensure there's visible content */}
+    <div className="flex">
+      <Sidebar />
+      <div className="border border-black p-4 flex-grow">
+        <h1>Welcome to the Merch Page!</h1> {/* Ensure there's visible content */}
+      </div>
     </div>
   );
 };
