6cc7bad17cde0f6be37271cfdd1b37b265e92a22
Karolina Tchiling <kdt@my.yorku.ca>
2024-10-23 15:59:31 -0400

    init home page aka merch page

---
+++ frontend/src/pages/MerchPage.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const MerchPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Merch Page!</h1> {/* Ensure there's visible content */}
+    </div>
+  );
+};
+
+export default MerchPage;
