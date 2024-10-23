087dfbe73b8495e89b8ce36973f454a1c36bba76
Karolina Tchiling <kdt@my.yorku.ca>
2024-10-23 16:01:32 -0400

    Update theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Instrument Sans', 'sans-serif'],
+      },
+      colors: {
+        camel: ({ opacityValue }) => {
+          if (opacityValue !== undefined) {
+            return `rgba(212, 163, 115, ${opacityValue})`;
+          }
+          return 'rgba(212, 163, 115, 1)';  // Fully opaque if no opacity specified
+        },
+        tea: '#FAEDCD',
+      },
+    },
   },
   plugins: [],
 };
