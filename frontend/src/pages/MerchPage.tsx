55648c00c6136b242e123acc8d358c63c23f1cf8
Karolina Tchiling <kdt@my.yorku.ca>
2024-10-23 22:14:56 -0400

    Added products

--- frontend/src/pages/MerchPage.tsx
+++ frontend/src/pages/MerchPage.tsx
@@ -1,12 +1,53 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar';
+import CatalogProduct from '../components/CatalogProduct';
 
 const MerchPage: React.FC = () => {
+
+  const products = [
+    { id: 1, name: 'Product 1', cost: '$25', image: 'butterfly.jpg' },
+    { id: 2, name: 'Product 2', cost: '$35', image: 'butterfly.jpg' },
+    { id: 3, name: 'Product 3', cost: '$45', image: 'butterfly.jpg' },
+    { id: 1, name: 'Product 1', cost: '$25', image: 'butterfly.jpg' },
+    { id: 2, name: 'Product 2', cost: '$35', image: 'butterfly.jpg' },
+    { id: 3, name: 'Product 3', cost: '$45', image: 'butterfly.jpg' },
+    { id: 1, name: 'Product 1', cost: '$25', image: 'butterfly.jpg' },
+    { id: 2, name: 'Product 2', cost: '$35', image: 'butterfly.jpg' },
+    { id: 3, name: 'Product 3', cost: '$45', image: 'butterfly.jpg' },
+    // Add more products here
+  ];
+
   return (
-    <div className="flex">
+    <div className="flex mt-4">
       <Sidebar />
-      <div className="border border-black p-4 flex-grow">
-        <h1>Welcome to the Merch Page!</h1> {/* Ensure there's visible content */}
+      <div className="flex-grow ml-[45px] mr-[200px]">
+
+        <div className="text-2xl">
+          Page Name
+        </div>
+
+        <div className="flex justify-between pt-5">
+          {/* Heading section */}
+          <div>
+            Showing pageMin - pageMax of total products
+          </div>
+          <div>
+            Sort by: Price(low to high)
+          </div>
+        </div>
+
+        {/* Product section */}
+        <div className="grid grid-cols-4 gap-8 pt-2">
+          {products.map((product) => (
+            <CatalogProduct
+              key={product.id}
+              name={product.name}
+              cost={product.cost}
+              image={product.image}
+            />
+          ))}
+        </div>
+
       </div>
     </div>
   );
