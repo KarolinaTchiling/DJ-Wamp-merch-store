f14f512fec1b0b4362e749221150b8e12ce502c9
JUB_Yoush <yoush1337@gmail.com>
2024-11-03 20:30:39 -0500

    extra user fields

--- backend/app/models.py
+++ backend/app/models.py
@@ -2,8 +2,12 @@ from mongoengine import *
 
 
 class User(Document):
+    fname = StringField(required=True)
+    lname = StringField(required=True)
     email = StringField(required=True)
     password = StringField(required=True)
+    cc_number = StringField()
+    cc_number = StringField()
 
     def __str__(self):
         return self.username
