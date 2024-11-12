d51a955150ea4c79769d151eca0fee56fe0b7b6f
Karolina Tchiling <kdt@my.yorku.ca>
2024-11-11 23:35:12 -0500

    Removed unused import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import TestPage from './pages/TestPage';
 import LogInPage from "./pages/LogInPage.tsx";
