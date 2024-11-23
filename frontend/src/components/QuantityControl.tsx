935cd23fd6958c5e817995779080c294fd01ac35
Karolina Tchiling <kdt@my.yorku.ca>
2024-11-23 12:57:32 -0500

    Moved state control to parent -> detailPage

---
+++ frontend/src/components/QuantityControl.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface QuantityControlProps {
+    quantity: number;
+    setQuantity: (value: number) => void;
+}
+const QuantityControl: React.FC<QuantityControlProps> = ({ quantity, setQuantity }) => {
+    const increment = () => setQuantity(quantity + 1);
+    const decrement = () => setQuantity(quantity > 0 ? quantity - 1 : 0);
+
+    return (
+        <div className="flex items-center space-x-4">
+            <p>Quantity:</p>
+            <button onClick={decrement} className="px-4 py-2 text-camel">-</button>
+            <span>{quantity}</span>
+            <button onClick={increment} className="px-4 py-2 text-camel">+</button>
+        </div>
+    );
+};
+
+export default QuantityControl;
