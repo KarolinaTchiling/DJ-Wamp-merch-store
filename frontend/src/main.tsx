ef5d64af89a6b7f6eca29d405228c1dd11432f4a
Karolina Tchiling <kdt@my.yorku.ca>
2024-11-24 19:39:46 -0500

    Implemented a global state for the the user token

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
+import { TokenProvider } from './TokenContext'; 
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
@@ -8,7 +8,9 @@ import { BrowserRouter } from 'react-router-dom';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <TokenProvider>
         <App />
+      </TokenProvider>
     </BrowserRouter>
   </StrictMode>,
 );
