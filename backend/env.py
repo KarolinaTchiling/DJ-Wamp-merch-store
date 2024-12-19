90118ef0f6be9215e640f10b56cde1cac923918a
JUB_Yoush <yoush1337@gmail.com>
2024-12-19 16:58:39 -0500

    no more env in repo

--- backend/env.py
+++
@@ -1,5 +0,0 @@
-USER = "admin"
-PASS = "wamp321"
-SECRET_KEY = "wampjwtkey"
-CC_ENCRYPT_KEY = "wampcreditcardkey"
-DB_URI = f"mongodb+srv://{USER}:{PASS}@cluster0.kiguh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
